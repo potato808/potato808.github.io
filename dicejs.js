7670e23304dd0478b13139b1026c633114bda906
potato808 <71228470+potato808@users.noreply.github.com>
2020-09-13 13:44:30 -0500

    Update dicejs.js

--- dicejs.js
+++ dicejs.js
@@ -2,7 +2,7 @@
 
 
 
-// var dice = ["dice1.png", "dice2.png", "dice3.png", "dice4.png", "dice5.png", "dice6.png"]
+
 
 document.querySelector(".roll").onclick = function() {rolldice()};
 function rolldice() {
